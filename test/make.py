06ef8ef9ef0b9bd00cbd5bcb851647e3f88d328e
Douglas Wu <wckdouglas@gmail.com>
2021-12-08 04:10:50 -0500

    fix 0-base

--- test/make.py
+++ test/make.py
@@ -5,4 +5,4 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from lifting_bam import make_ref_fasta
 
-print(make_ref_fasta(sys.argv[1], chrom="chr3", start=30, stop=46, padding=2))
+print(make_ref_fasta(sys.argv[1], chrom="chr3", start=29, stop=46, padding=2))
