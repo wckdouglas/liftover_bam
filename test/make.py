1bc046d931a1b8d9757f9898a2b9cafd26153b2a
Douglas Wu <wckdouglas@gmail.com>
2022-08-07 09:54:06 -0400

    fixed test

--- test/make.py
+++ test/make.py
@@ -1,9 +1,6 @@
-import os
 import sys
 from pathlib import Path
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from lifting_bam import make_ref_fasta
+from liftover_bam import make_ref_fasta
 
 print(make_ref_fasta(Path(sys.argv[1]), chrom="chr3", start=29, stop=46, padding=2))
